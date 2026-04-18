560933497cf482d014d437bf78a0cdb3034be305
sawa <savabondarenko2016@gmail.com>
2026-04-18 12:02:27 +0300

    change db

--- src/core/utils/vectorstore.py
+++ src/core/utils/vectorstore.py
@@ -1,27 +1,42 @@
-import faiss
-import numpy as np
+from langchain_qdrant import QdrantVectorStore
+from langchain_core.documents import Document
+from qdrant_client import QdrantClient
+from qdrant_client.http.models import Distance, VectorParams
 import logging
+import os
+
 
 logger = logging.getLogger(__name__)
+
+db_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "qdrant_data")
+client = QdrantClient(path=db_path)
+
 def create_vectorDB(embeddings: np.ndarray) -> faiss.Index:
-    """Create faiss index from embeddings.
+    """Create or update Qdrant vector store from documents.
 
     Args:
-        embeddings (np.ndarray): 2D numpy array with shape (n, d).
+        documents (list[Document]): splitted documents.
+        embedder: object for creating embedding vectors.
 
     Returns:
-        faiss.Index: faiss index(vector DB object).
+        QdrantVectorStore: Vector store object.
 
     Raises:
-        Exception: If failed to create faiss index.
+        Exception: if something went wrong.
     """
+    collection_name = "demo_collection"
     try:
-        dimension = embeddings.shape[1]
-        vectorstore = faiss.IndexFlatIP(dimension)
-        vectorstore.add(embeddings.astype(np.float32))
-        logger.info("Created faiss vectorstore")
-        return vectorstore
+       vector_store = QdrantVectorStore.from_documents(
+            documents=documents,
+            embedding=embedder,
+            url=db_path,
+            collection_name=collection_name,
+            client=client
+        )
+
+       logger.info("Successfully created/updated Qdrant vectorstore")
+       return vector_store
     except Exception as e:
-        logger.critical("Failed to create vectorDB")
-        raise e
+       logger.critical(f"Failed to create vectorDB: {e}")
+       raise e
 
