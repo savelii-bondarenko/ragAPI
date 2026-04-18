e5a8c3fe94b181dff752ee21d6854402a1f9b63d
sawa <savabondarenko2016@gmail.com>
2026-04-18 11:37:17 +0300

    add some async

--- src/core/engine.py
+++ src/core/engine.py
@@ -1,13 +1,15 @@
 from langchain_core.documents import Document
 from faiss import Index
 
+import asyncio
+
 from .utils.data_reader import read_data
 from .utils.split_text import split_text
 from .utils.create_embeddings import shared_embedder
 from .utils.vectorstore import create_vectorDB
 
 
-def prepare_rag_assets(file_path: str):
+async def prepare_rag_assets(file_path: str):
     """Prepare data for RAG
 
         Args:
@@ -20,10 +22,9 @@ def prepare_rag_assets(file_path: str):
 
             vector_db (faiss.Index): vector database.
     """
-    extracted_text = read_data(file_path)
-    splitted_text = split_text(extracted_text)
-
-    embeddings = shared_embedder.make_embeddings(splitted_text)
-    vector_db = create_vectorDB(embeddings)
+    extracted_text = await asyncio.to_thread(read_data, file_path)
+    splitted_text = await asyncio.to_thread(split_text, extracted_text)
+    embeddings = await asyncio.to_thread(shared_embedder.make_embeddings, splitted_text)
+    vector_db = await asyncio.to_thread(create_vectorDB, embeddings)
 
     return splitted_text, shared_embedder, vector_db
