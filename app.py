746fa892ed732ddedb347239eb287d39f04d9dae
sawa <savabondarenko2016@gmail.com>
2026-04-28 16:46:45 +0300

    some refactor app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ app = FastAPI(title="Python RAG Worker")
 app.include_router(router)
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000)
